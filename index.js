86bc290acf496d6e072ae37ef362c492b75ae96d
EricAlexanderZ <ericalexander0514@gmail.com>
2022-03-09 10:11:02 -0600

    Finished project one

---
+++ index.js
@@ -0,0 +1,26 @@
+// PSEUDOCODE: 
+
+// WE NEED TO CREATE  COUNT & GIVE IT A VALUE OF 0
+// INCREMENT COUNT += 1;  / MAKE IT REUSABLE
+// MAKE INCREMENT BTN FUNCTIONAL ONCLICK
+
+// ADD FUNCTIONALITY TO SAVE BTN 
+// LOG NUMBERS TO LOG PARAGRAPH 
+// REST COUNT TO 0
+
+let count = 0
+let countEl = document.getElementById("count_el")
+let logPara = document.getElementById("log_para")
+
+function addOne() {
+    count++
+    countEl.textContent = count
+}
+document.getElementById("increment_btn").addEventListener("click", addOne);
+
+function save() {
+    logPara.textContent += `${count} - `;
+    let reset = 0
+    countEl.textContent = reset;
+}
+document.getElementById("save_btn").addEventListener("click", save);
